ebba52452abc041f5476f5ff2dd5d2102fae7463
mrBidone <ab.bz@ukr.net>
2024-06-12 00:57:25 +0200

    Add timer with styles

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,27 +1,20 @@
-// commonjs
-const flatpickr = require("flatpickr");
-// Описаний в документації
+
+
 import flatpickr from "flatpickr";
-// Додатковий імпорт стилів
 import "flatpickr/dist/flatpickr.min.css";
 
-// Описаний у документації
 import iziToast from "izitoast";
-// Додатковий імпорт стилів
 import "izitoast/dist/css/iziToast.min.css";
-
-const options = {
-  enableTime: true,
-  time_24hr: true,
-  defaultDate: new Date(),
-  minuteIncrement: 1,
-  onClose(selectedDates) {
-    console.log(selectedDates[0]);
-  },
+import imageUrl from '../img/Error-icon.svg';
+const refs = {
+    inputDate: document.querySelector('#datetime-picker'),
+    startBtn: document.querySelector('[data-start]'),
+    timerContainer: document.querySelector('.timer'),
+    daysCounter: document.querySelector('[data-days]'),
+    hoursCounter: document.querySelector('[data-hours]'),
+    minCounter: document.querySelector('[data-minutes]'),
+    secCounter: document.querySelector('[data-seconds]')
 };
-
-// Функція конвертації мілісекунд в часові значення 
-//  ms — різниця між кінцевою і поточною датою в мілісекундах.
 
 function convertMs(ms) {
   // Number of milliseconds per unit of time
@@ -37,11 +30,76 @@ function convertMs(ms) {
   // Remaining minutes
   const minutes = Math.floor(((ms % day) % hour) / minute);
   // Remaining seconds
-  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
-
+    const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+    
   return { days, hours, minutes, seconds };
 }
 
-console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
-console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
-console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
+refs.startBtn.disabled = true;
+
+let userSelectedDate;
+let currentDate = Date.now();
+let intervalId;
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+      console.log(selectedDates[0]);
+      userSelectedDate = selectedDates[0];
+      
+      if (userSelectedDate < currentDate) {
+          refs.startBtn.disabled = true;
+          iziToast.show({
+              title: 'Error',
+              titleSize: '16px',
+              titleLineHeight: '1.5',
+              titleColor: '#FFFFFF',
+                message: "Please choose a date in the future",
+              messageSize: '16px',
+                messageLineHeight: '1.5',
+                backgroundColor: '#EF4040',
+                messageColor: '#FFFFFF',
+              iconUrl: imageUrl,
+              position: 'topRight',
+              progressBarColor: '#B51B1B',
+            theme: 'dark'
+});
+      } else {
+          refs.startBtn.disabled = false;
+      }; 
+  },
+};
+
+
+
+flatpickr(refs.inputDate, options);
+
+refs.startBtn.addEventListener('click', () => {
+    console.log('start');
+    refs.startBtn.disabled = true;
+    refs.inputDate.disabled = true;
+    intervalId = setInterval(() => {
+        const diff = userSelectedDate - Date.now();
+        const time = convertMs(diff);
+        refs.daysCounter.textContent = formatAsTwoDigits(time.days);
+        refs.hoursCounter.textContent = formatAsTwoDigits(time.hours);
+        refs.minCounter.textContent = formatAsTwoDigits(time.minutes);
+        refs.secCounter.textContent = formatAsTwoDigits(time.seconds);
+    }, 1000)
+
+    setTimeout(() => {
+    clearInterval(intervalId);
+    refs.inputDate.disabled = false;
+    }, userSelectedDate - Date.now());
+    
+});
+
+
+
+const formatAsTwoDigits = (value => value.toString().padStart(2, "0"));
+
+
+
